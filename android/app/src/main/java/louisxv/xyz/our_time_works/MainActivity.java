5e7a70bf3303e4c105c444af9923943ef8c9b836
LouisXVBTW <41397906+LouisXVBTW@users.noreply.github.com>
2020-03-17 21:24:58 +0000

    Add files via upload

---
+++ android/app/src/main/java/louisxv/xyz/our_time_works/MainActivity.java
@@ -0,0 +1,13 @@
+package louisxv.xyz.our_time_works;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
